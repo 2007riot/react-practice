23db63fa560a07a8564446517505c4b18321ec65
2007riot <2007riot@gmail.com>
2024-02-16 12:54:09 +0100

    random color implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Accoridion from './components/accoridion'
+import RandomColor from './components/random-color'
 
 function App() {
 
   return (
     <div className="App">
-      <Accoridion />
+      {/* Accoridion component */}
+      {/* <Accoridion /> */}
+
+      {/* Random color component */}
+      <RandomColor />
     </div>
   )
 }
