c28db4e5116de0d79cc36109211724bbdccd5e27
2007riot <2007riot@gmail.com>
2024-03-07 12:54:21 +0100

    load-more-data-completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Accoridion from './components/accoridion'
 import ImageSlider from './components/image-slider'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
+import LoadMoreData from './components/load-more-data'
 
 function App() {
 
@@ -16,7 +17,9 @@ function App() {
       {/* Star Rating Component */}
       {/* <StarRating numberOfStars={10}/> */}
       {/* Image slider component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} page={"1"}/> 
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} page={"1"}/>  */}
+      {/* Load more data  */}
+      <LoadMoreData />
 
     </div>
   )
